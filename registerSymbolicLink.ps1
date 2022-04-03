4b5a56f0036680147e9ecde5e243d64aba973732
Nathan Clouse <nclouse@envistacorp.com>
2022-04-03 01:45:01 +0000

    initial commit

---
+++ registerSymbolicLink.ps1
@@ -0,0 +1,10 @@
+﻿# from https://github.com/vxzry/d365fo-template/blob/main/registerSymbolicLink.ps1
+$AOSMetadataPath = "k:\AOSService\PackagesLocalDirectory"
+$RepoPath = "C:\Users\Admin4b2aaec26a\source\repos\NathanClouseAX\FOMultiPackageEasyWay"
+$RepoMetadataPath = $RepoPath + "\Metadata"
+$RepoModelFolders = Get-ChildItem $RepoMetadataPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	$Target = "$RepoMetadataPath\$ModelFolder"
+	New-Item -ItemType SymbolicLink -Path "$AOSMetadataPath" -Name "$ModelFolder" -Value "$Target"
+}
---
+++ unregisterSymbolicLink.ps1
@@ -0,0 +1,9 @@
+﻿# from https://github.com/vxzry/d365fo-template/blob/main/unregisterSymbolicLink.ps1
+$AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+$RepoPath = "C:\Users\Admin4b2aaec26a\source\repos\NathanClouseAX\FOMultiPackageEasyWay"
+$RepoMetadataPath = $RepoPath + "\Metadata"
+$RepoModelFolders = Get-ChildItem $RepoMetadataPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	cmd /c rmdir "$AOSMetadataPath\$ModelFolder"
+}
